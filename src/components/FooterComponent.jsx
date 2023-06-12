9ea878b10c121387ec435a00d63c8f541a825992
nicolas l <nicolaslandazabal@hotmail.com>
2023-06-12 18:08:07 -0500

    change: change url code

--- src/components/FooterComponent.jsx
+++ src/components/FooterComponent.jsx
@@ -3,7 +3,11 @@ export const FooterComponent = () => {
     <div>
       <p>Designed and coded by : Nicolas L.B.</p>
       <p>Code</p>
-      <a target="_blank" rel="noreferrer" href="https://www.github.com">
+      <a
+        target="_blank"
+        rel="noreferrer"
+        href="https://github.com/nicolanda/fcc-25-clock"
+      >
         Github
       </a>
     </div>
